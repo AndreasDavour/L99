792570dfc35be32190d5a3a618bf1657309829e8
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-04-16 16:32:10 +0200

    I give up. time to analyze some working examples. Pushing those

--- L07.lisp
+++ L07.lisp
@@ -1,67 +1,29 @@
 ;;;; L07 - Flatten a nested list structure.
 
-(defun my-flatten (lst)
-  (let ((tmp nil))
-    (labels ((recurse-over-list (lst2)
-	       (cond
-		 ((atom (car lst2))
-		  (format t "atom~%")
-		  (push (car lst2) tmp)
-		  (recurse-over-list (cdr lst2)))
-		 ((listp (car lst2))
-		  (format t "list")
-		  (recurse-over-list (car lst2)))
-		 (nil nil))))
-      (recurse-over-list lst))
-    tmp))
+(defun my-flatten (x)
+  (labels ((rec (x acc)
+	     (cond
+	       ((null x) acc)
+	       ((atom x) (cons x acc))
+	       (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
 
-(defun my-flatten (lst)
-  (let ((tmp nil))
-    (labels ((recurse-over-list (lst2)
-	       (if (atom (car lst2))
-		   (progn
-		     (format t "atom~%")
-		     (push (car lst2) tmp)
-		     (recurse-over-list (cdr lst2))))
-	       (if (listp (car lst2))
-		   (progn
-		     (format t "list")
-		     (recurse-over-list (car lst2))))
-	       (if (equal (cdr lst2) nil)
-		   nil)))
-      (recurse-over-list lst))
-    tmp))
+(defun my-flatten (x)
+  (rec x nil))
 
-(defparameter tmp nil)
+(defun rec (x acc)
+    (cond
+      ((null x) acc)
+      ((atom x) (cons x acc))
+      (t (rec (car x) (rec (cdr x) acc)))))
 
-(defun my-flatten (lst)
-  (if (null (car lst))
-	nil
-	(progn
-	  (push-atoms lst)
-	  (print tmp)
-	  (my-flatten (cdr lst)))))
+;;;;;;;;;;;;;;;;
 
-(defun push-atoms (lst2)
-  (format t "lst2:~A collector:~A~%" lst2 tmp)
-  (if (zerop (car lst2))
+(defun my-flatten (orig-list)
+  (if (eql orig-list nil)
       nil
-      (push (car lst2) tmp)))
-
-
-(defun flatten-helper (lst2)
-    (if (null (car lst2))
-	nil
-	(cond
-	  ((atom (car lst2))
-	   (push (car lst2) tmp)
-	   (flatten-helper (cdr lst2)))
-	  ((listp (car lst2))
-	   (append (car lst2) tmp)))))
-
-(defun my-flatten (lst)
-  (let ((tmp nil))
-    (flatten-helper lst)
-    tmp))
-
-
+      (let ((elem (car orig-list))
+	    (resto-list (cdr orig-list)))
+	(if (listp elem)
+	    (append (my-flatten elem) (my-flatten resto-list))
+	    (append (cons elem nil) (my-flatten resto-list))))))
