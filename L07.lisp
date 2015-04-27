25793feb2e505633820e15da654cfe3154cd449b
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-04-27 13:28:16 +0200

    cleared up who did what on L07 for now. will get back to it later

--- L07.lisp
+++ L07.lisp
@@ -1,5 +1,6 @@
 ;;;; L07 - Flatten a nested list structure.
 
+;;; Paul Graham's solution
 (defun my-flatten (x)
   (labels ((rec (x acc)
 	     (cond
@@ -17,8 +18,7 @@
       ((atom x) (cons x acc))
       (t (rec (car x) (rec (cdr x) acc)))))
 
-;;;;;;;;;;;;;;;;
-
+;;; someone elses solution
 (defun my-flatten (orig-list)
   (if (eql orig-list nil)
       nil
@@ -27,3 +27,5 @@
 	(if (listp elem)
 	    (append (my-flatten elem) (my-flatten resto-list))
 	    (append (cons elem nil) (my-flatten resto-list))))))
+
+;;; my solution
