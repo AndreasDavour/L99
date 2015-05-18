9ac4c931b7beed4ab84d6b3654bb19ba7392fdbd
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-05-18 15:57:20 +0200

    L10 completed with PACK from L09

--- L10.lisp
+++ L10.lisp
@@ -3,7 +3,33 @@
 ;; 
 ;; ((a a a a) (b) (c c) (a a) (d) (e e e e))
 
+;; dirty solution from L09
+(defun pack (lst)
+  "Correctly packs repeated symbols into sublists."
+  (let ((latest (car lst))
+	(acc nil)
+	(result nil)
+	(counter (length lst)))
+    (mapcar #'(lambda (x)
+		(setf counter (- counter 1))
+		(if (equal 0 counter)
+		    (progn
+		      (setf acc (cons x acc))
+		      (setf result (cons acc result)))
+		    (if (equal x latest)
+			(progn
+;;			  (format t "equal, x: ~A acc: ~A latest: ~A~%" x acc latest)
+			  (setf acc (cons x acc)))
+			(progn
+;;			  (format t "not equal, x: ~A acc: ~A latest: ~A~%" x acc latest)
+			  (setf result (cons acc result))
+			  (setf latest x)
+			  (setf acc nil)
+			  (setf acc (cons x acc))))))
+	    lst)
+    (nreverse result)))
+
 (defun compress (lst)
   (mapcar #'(lambda (x)
-	      (list (car x) (length x)))
-	  lst))
+	      (list (length x) (car x)))
+	  (pack lst)))
