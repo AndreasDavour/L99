8c1651ed9e63262da912fe81c11fa04ab4bbd115
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-05-18 15:52:15 +0200

    finally managed an interative, but inelegant, solution to L09

--- L09.lisp
+++ L09.lisp
@@ -6,60 +6,62 @@
 ;;; * (pack '(a a a a b c c a a d e e e e))
 ;;;     ((A A A A) (B) (C C) (A A) (D) (E E E E))
 
-(defun pack (x)
-  (let ((lst (car x)))
-    (loop for i in x 
-	  until (not (equal i lst))
-	  collecting i)))
 
-(defun pack (x)
-  (let ((tmp nil)
-	(last (first x)))
-    (loop for i in x
-	  for former = last
-	  do (setf last i)
-	  if (equal i former)
-	    do (push i tmp)
-	  else
-	    do (progn 
-		 (setf tmp (list tmp))
-		 (push i tmp))
-	  end)
-    tmp))
+;;; Interative solution
+;;; really kludgy, needs cleanup
+;;; suggestion: use COND to make the conditionals more c;ear
 
-;;; how to recurse
-;; this is our end state
-; (cons (cons 'a (cons 'a (cons 'a (cons 'a nil)))) (cons (cons 'b nil) (cons (cons 'c (cons 'c nil)) (cons (cons 'a (cons 'a nil)) (cons (cons 'd nil) (cons 'e (cons 'e (cons 'e (cons 'e nil)))))))))
-
-
-;; (let ((tmp (car lst)))
-;;   (labels ((helper (y acc) 
-;; 	     (cond
-;; 	       ((equal (cdr y) nil)
-;; 		nil)
-;; 	       ((equal tmp (car y))
-;; 		(cons (car y) (helper (cdr y) acc))))))
-;;     (helper lst tmp)))
+(defun pack (lst)
+  "Correctly packs repeated symbols into sublists."
+  (let ((latest (car lst))
+	(acc nil)
+	(result nil)
+	(counter (length lst)))
+    (mapcar #'(lambda (x)
+		(setf counter (- counter 1))
+		(if (equal 0 counter)
+		    (progn
+		      (setf acc (cons x acc))
+		      (setf result (cons acc result)))
+		    (if (equal x latest)
+			(progn
+;;			  (format t "equal, x: ~A acc: ~A latest: ~A~%" x acc latest)
+			  (setf acc (cons x acc)))
+			(progn
+;;			  (format t "not equal, x: ~A acc: ~A latest: ~A~%" x acc latest)
+			  (setf result (cons acc result))
+			  (setf latest x)
+			  (setf acc nil)
+			  (setf acc (cons x acc))))))
+	    lst)
+    (nreverse result)))
 
 
-(defun pack (x)
-  (let* ((tmp (car x)))
-    (pack-helper x tmp)))
-	 
 
-(defun pack-helper (y acc)
-  (cond
-    ;; if end of list, cdr is nil
-    ((equal (cdr y) nil)
-     nil)
-    ;; last car and present car is the same
-    ((equal acc (car y))
-     (cons (car y) (pack-helper (cdr y) acc)))
-    (t
-     ;; (A A A A (B) B (C) C C (A) A A (D) D (E) E E E)
-     ;; (cons (cons (car y) nil) (pack-helper y (car y))))))
-     ;; (A A A A (B) (C) C (A) A (D) (E) E E)
-     ;;
-     ;; last car and present car is not the same
-     (cons (cons (car y) nil) (pack-helper (cdr y) (car y))))))
-
+;;; ;;; NON WORKING RECURSIVE ATTEMPT
+;;; ;;;
+;;; ;;; how to recurse
+;;; ;; this is our end state
+;;; ; (cons (cons 'a (cons 'a (cons 'a (cons 'a nil)))) (cons (cons 'b nil) (cons (cons 'c (cons 'c nil)) (cons (cons 'a (cons 'a nil)) (cons (cons 'd nil) (cons 'e (cons 'e (cons 'e (cons 'e nil)))))))))
+;;; 
+;;; 
+;;; (defun pack (x)
+;;;   (let* ((tmp (car x)))
+;;;     (pack-helper x tmp)))
+;;; 	 
+;;; 
+;;; (defun pack-helper (y acc)
+;;;   (cond
+;;;     ;; if end of list, cdr is nil
+;;;     ((equal (cdr y) nil)
+;;;      nil)
+;;;     ;; last car and present car is the same
+;;;     ((equal acc (car y))
+;;;      (cons (car y) (pack-helper (cdr y) acc)))
+;;;     (t
+;;;      ;; (A A A A (B) B (C) C C (A) A A (D) D (E) E E E)
+;;;      ;; (cons (cons (car y) nil) (pack-helper y (car y))))))
+;;;      ;; (A A A A (B) (C) C (A) A (D) (E) E E)
+;;;      ;;
+;;;      ;; last car and present car is not the same
+;;;      (cons (cons (car y) nil) (pack-helper (cdr y) (car y))))))
