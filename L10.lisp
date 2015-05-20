c0cb53e0c45227fff99528000f415fd89732c8e8
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-05-20 17:33:10 +0200

    Added doc string and example to L10

--- L10.lisp
+++ L10.lisp
@@ -29,7 +29,12 @@
 	    lst)
     (nreverse result)))
 
-(defun compress (lst)
+(defun encode (lst)
+  "Frequency compacts elements of a list"
   (mapcar #'(lambda (x)
 	      (list (length x) (car x)))
 	  (pack lst)))
+
+;; (a a a a b c c a a d e e e e) =>
+;; ((4 A) (1 B) (2 C) (2 A) (1 D) (4 E))
+
