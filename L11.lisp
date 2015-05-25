60b08525129dcc8884d4f095470b15a147b090dc
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-05-25 16:37:23 +0200

    working solution for L11 but the ugly solution for 09 is adding up ugly! :(

--- L11.lisp
+++ L11.lisp
@@ -8,3 +8,46 @@
 ;; * (encode-modified '(a a a a b c c a a d e e e e))
 ;; ((4 A) B (2 C) (2 A) D (4 E))
 
+;; dirty solution from L09
+(defun pack (lst)
+  "Correctly packs repeated symbols into sublists."
+  (let ((latest (car lst))
+	(acc nil)
+	(result nil)
+	(counter (length lst)))
+    (mapcar #'(lambda (x)
+		(setf counter (- counter 1))
+		(if (equal 0 counter)
+		    (progn
+		      (setf acc (cons x acc))
+		      (setf result (cons acc result)))
+		    (if (equal x latest)
+			(progn
+;;			  (format t "equal, x: ~A acc: ~A latest: ~A~%" x acc latest)
+			  (setf acc (cons x acc)))
+			(progn
+;;			  (format t "not equal, x: ~A acc: ~A latest: ~A~%" x acc latest)
+			  (setf result (cons acc result))
+			  (setf latest x)
+			  (setf acc nil)
+			  (setf acc (cons x acc))))))
+	    lst)
+    (nreverse result)))
+
+(defun encode (lst)
+  "Frequency compacts elements of a list"
+  (let* ((packed-list (pack lst))
+	 (slimlist nil))
+    ;;; UGLY post processing hack! PACK should be refactored!
+    ;; make sublists except for the singles
+    (dolist (tmp packed-list)
+      (if (equal (length tmp) 1)
+	  (push (car tmp) slimlist)
+	  (push tmp slimlist)))
+    ;; now count them, unless atoms
+    (mapcar #'(lambda (x)
+		(if (atom x)
+		    x
+		    (list (length x) (car x))))
+	    (nreverse slimlist))))
+
