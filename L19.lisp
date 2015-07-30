3e02d0b5f4ff861c6c46d0d5c3d12e8816119489
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-07-30 16:40:24 +0200

    L19: working solution, with recursive helpers.

--- L19.lisp
+++ L19.lisp
@@ -7,3 +7,21 @@
 ;;; * (rotate '(a b c d e f g h) -2)
 ;;; (G H A B C D E F)
 
+(defun rotate (lst shift)
+  (let ((overflow))
+    (labels ((helper-positive (data acc)
+	       (if (= acc 0)
+		   (setf overflow (append data (nreverse overflow)))
+		   (progn
+		    (push (car data) overflow)
+		    (helper-positive (cdr data) (- acc 1)))))
+	     (helper-negative (data acc)
+	       (if (= acc 0)
+		   (setf overflow (append overflow data))
+		   (progn
+		     (push (car (reverse data)) overflow)
+		     (helper-negative (reverse (cdr (reverse data))) (- acc 1))))))
+      (if (> shift 0)
+	  (helper-positive lst shift)
+	  (helper-negative lst (abs shift)))
+      overflow)))
